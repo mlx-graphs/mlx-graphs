832ab267217aa932c73fd7ca5c9a9edd449ad528
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-12 00:35:07 +0100

    ✅ tests for message passing + some for scatter_softmax (need more)

---
+++ tests/utils/test_scatter.py
@@ -0,0 +1,14 @@
+import mlx.core as mx
+import pytest
+
+from mlx_graphs.utils import scatter_softmax
+
+
+def test_scatter_softmax():
+    src = mx.array([1., 1., 1., 1.])
+    index = mx.array([0, 0, 1, 2])
+    
+    y_hat = scatter_softmax(src, index, src.shape)
+    y = mx.array([0.5, 0.5, 1, 1])
+
+    assert mx.all(y_hat == y), "Simple scatter Softmax failed"
