89fc98b16f86dc79fc6a15cc33869c8100139419
Francesco Farina <farina.francesco1991@gmail.com>
2024-01-15 10:06:31 +0000

    add inverst transformation

--- mlx_graphs/utils/transformations.py
+++ mlx_graphs/utils/transformations.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import mlx.core as mx
 import numpy as np
 
@@ -79,3 +80,51 @@ def to_sparse_adjacency_matrix(adjacency_matrix: mx.array) -> tuple[mx.array, mx
     edge_index = to_edge_index(adjacency_matrix)
     edge_features = adjacency_matrix[edge_index[0], edge_index[1]]
     return edge_index, edge_features
+
+
+def to_adjacency_matrix(
+    edge_index: mx.array, num_nodes: int, edge_features: Optional[mx.array] = None
+) -> mx.array:
+    """
+    Converts an edge index representation to an adjacency matrix.
+
+    Args:
+        edge_index (mlx.core.array): a [2, num_edges] array representing the source and target nodes of each edge
+        num_nodes (int): the number of nodes in the graph
+        edge_features (mlx.core.array, optional): edge features corresponding to the edges in edge_index. Defaults to None.
+
+    Returns:
+        mlx.core.array: the resulting adjacency matrix
+    """
+    if mx.max(edge_index) > num_nodes - 1:
+        raise ValueError(
+            "num_nodes must be >= than the number of nodes in the edge_index ",
+            f"(got num_nodes={num_nodes} and {mx.max(edge_index) + 1} nodes in index",
+        )
+    if edge_index.ndim != 2:
+        raise ValueError(
+            "edge_index must be 2-dimensional with shape [2, num_edges]",
+            f"(got {edge_index.ndim} dimensions)",
+        )
+    if edge_index.shape[0] != 2:
+        raise ValueError(
+            "edge_index must be 2-dimensional with shape [2, num_edges]",
+            f"(got {edge_index.shape} shape)",
+        )
+
+    if edge_features is not None:
+        if edge_features.ndim != 1:
+            raise ValueError(
+                f"edge_features must be 1-dimensional (got {edge_features.ndim} dimensions)"
+            )
+        if edge_index.shape[1] != edge_features.shape[0]:
+            raise ValueError(
+                "edge_features must be 1 per edge ",
+                f"(got {edge_index.shape[1]} edges and {edge_features.shape[0]} features)",
+            )
+    adjacency_matrix = mx.zeros((num_nodes, num_nodes), dtype=edge_index.dtype)
+    if edge_features is None:
+        adjacency_matrix[edge_index[0], edge_index[1]] = 1
+    else:
+        adjacency_matrix[edge_index[0], edge_index[1]] = edge_features
+    return adjacency_matrix
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -3,6 +3,7 @@ from mlx_graphs.utils.transformations import (
     to_edge_index,
     to_sparse_adjacency_matrix,
     check_adjacency_matrix,
+    to_adjacency_matrix,
 )
 import pytest
 
@@ -47,3 +48,53 @@ def test_to_sparse_adjacency_matrix():
     assert mx.array_equal(edge_index, expected_index)
     expected_features = mx.array([1, 2, 3, 5, 1, 2])
     assert mx.array_equal(edge_features, expected_features)
+
+
+def test_to_adjacency_matrix():
+    edge_index = mx.array([[0, 0, 1, 2, 2, 2], [1, 2, 0, 0, 1, 2]])
+    edge_features = mx.array([1, 2, 3, 5, 1, 2])
+
+    # 3 nodes
+    num_nodes = 3
+    expected_binary_matrix = mx.array([[0, 1, 1], [1, 0, 0], [1, 1, 1]])
+    adj_matrix = to_adjacency_matrix(edge_index, num_nodes)
+    assert mx.array_equal(expected_binary_matrix, adj_matrix)
+    expected_matrix = mx.array([[0, 1, 2], [3, 0, 0], [5, 1, 2]])
+    weighted_adj_matrix = to_adjacency_matrix(edge_index, num_nodes, edge_features)
+    assert mx.array_equal(expected_matrix, weighted_adj_matrix)
+
+    # 4 nodes (extra padding)
+    num_nodes = 4
+    expected_binary_matrix = mx.array(
+        [[0, 1, 1, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
+    )
+    adj_matrix = to_adjacency_matrix(edge_index, num_nodes)
+    assert mx.array_equal(expected_binary_matrix, adj_matrix)
+    expected_matrix = mx.array([[0, 1, 2, 0], [3, 0, 0, 0], [5, 1, 2, 0], [0, 0, 0, 0]])
+    weighted_adj_matrix = to_adjacency_matrix(edge_index, num_nodes, edge_features)
+    assert mx.array_equal(expected_matrix, weighted_adj_matrix)
+
+    # 2 nodes (expect error as there are 3 in index)
+    with pytest.raises(ValueError):
+        to_adjacency_matrix(edge_index, num_nodes=2)
+    # non 2D edge_index
+    with pytest.raises(ValueError):
+        edge_index = mx.array([[[0, 0, 1, 2, 2, 2], [1, 2, 0, 0, 1, 2]]])
+        to_adjacency_matrix(edge_index, 3)
+
+    # column edge_index
+    with pytest.raises(ValueError):
+        edge_index = mx.array([[0, 1], [1, 2], [0, 2]])
+        to_adjacency_matrix(edge_index, 3)
+
+    # more features than edges
+    with pytest.raises(ValueError):
+        edge_index = mx.array([[0, 0, 1, 2, 2, 2], [1, 2, 0, 0, 1, 2]])
+        edge_features = mx.array([1, 2, 3, 5, 1, 2, 5])
+        to_adjacency_matrix(edge_index, 3, edge_features)
+
+    # less features than edges
+    with pytest.raises(ValueError):
+        edge_index = mx.array([[0, 0, 1, 2, 2, 2], [1, 2, 0, 0, 1, 2]])
+        edge_features = mx.array([1, 2, 3, 5, 1])
+        to_adjacency_matrix(edge_index, 3, edge_features)
