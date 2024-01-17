b1e11e63994fa9c60ead46cf4c5622218cb2b5f9
Francesco Farina <farina.francesco1991@gmail.com>
2024-01-17 17:00:17 +0000

    fix gnn block example

--- examples/graph_network_block.py
+++ examples/graph_network_block.py
@@ -1,7 +1,7 @@
 import mlx.core as mx
-from mlx.nn.base import Module
+from mlx.nn.layers.base import Module
 from mlx.nn.layers.linear import Linear
-from mlx.nn.layers.gnn import GraphNetworkBlock
+from mlx_graphs.nn.graph_network_block import GraphNetworkBlock
 
 
 class NodeModel(Module):
