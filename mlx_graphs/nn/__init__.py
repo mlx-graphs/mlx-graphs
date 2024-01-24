36d176e511bc0fa966792b29a217df1a9b74d62a
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-24 11:08:15 +0100

    match the benchmark on main

--- mlx_graphs/nn/__init__.py
+++ mlx_graphs/nn/__init__.py
@@ -1,4 +1,2 @@
-from .conv.gat_conv import GATConv  # noqa
-from .conv.gcn_conv import GCNConv  # noqa
-from .graph_network_block import GraphNetworkBlock  # noqa
-from .message_passing import MessagePassing  # noqa
+from .conv import *  # noqa
+from .linear import Linear  # noqa
