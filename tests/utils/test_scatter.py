1ab72fe5e13747d1b97536fba271a4ee478b6978
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-25 12:19:06 +0100

    use parameterized test for scatter

--- tests/utils/test_scatter.py
+++ tests/utils/test_scatter.py
@@ -4,49 +4,80 @@ import pytest
 from mlx_graphs.utils import scatter, degree
 
 
-def test_scatter():
-    src = mx.array([1.0, 1.0, 1.0, 1.0])
-    index = mx.array([0, 0, 1, 2])
-    num_nodes = src.shape[0]
+@pytest.mark.parametrize(
+    "src, index, num_nodes, aggr, expected",
+    [
+        # Test case for test_scatter
+        (
+            mx.array([1.0, 1.0, 1.0, 1.0]),
+            mx.array([0, 0, 1, 2]),
+            None,
+            "softmax",
+            mx.array([0.5, 0.5, 1, 1]),
+        ),
+        (
+            mx.array([1.0, 1.0, 1.0, 1.0]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "add",
+            mx.array([2, 1, 1]),
+        ),
+        (
+            mx.array([1.0, 1.0, 1.0, 1.0]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "max",
+            mx.array([1, 1, 1]),
+        ),
+        (
+            mx.array([1.0, 1.0, 1.0, 1.0]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "mean",
+            mx.array([1, 1, 1]),
+        ),
+        # Test case for test_scatter_2D
+        (
+            mx.array([[1.0, 2.0], [1.0, 3.0], [1.0, 4.0], [1.0, 5.0]]),
+            mx.array([0, 0, 1, 2]),
+            None,
+            "softmax",
+            mx.array([[0.5, 0.269], [0.5, 0.731], [1, 1], [1, 1]]),
+        ),
+        (
+            mx.array([[1.0, 2.0], [1.0, 3.0], [1.0, 4.0], [1.0, 5.0]]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "add",
+            mx.array([[2, 5], [1, 4], [1, 5]]),
+        ),
+        (
+            mx.array([[1.0, 2.0], [1.0, 3.0], [1.0, 4.0], [1.0, 5.0]]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "max",
+            mx.array([[1, 3], [1, 4], [1, 5]]),
+        ),
+        (
+            mx.array([[1.0, 2.0], [1.0, 3.0], [1.0, 4.0], [1.0, 5.0]]),
+            mx.array([0, 0, 1, 2]),
+            3,
+            "mean",
+            mx.array([[1, 2.5], [1, 4], [1, 5]]),
+        ),
+    ],
+)
+def test_scatter(src, index, num_nodes, aggr, expected):
+    if num_nodes is None:
+        num_nodes = src.shape[0] if aggr == "softmax" else index.max().item() + 1
+    y_hat = scatter(src, index, num_nodes, aggr=aggr)
 
-    y_hat1 = scatter(src, index, aggr="softmax")
-
-    num_nodes = index.max().item() + 1
-    y_hat2 = scatter(src, index, num_nodes, aggr="add")
-    y_hat3 = scatter(src, index, num_nodes, aggr="max")
-    y_hat4 = scatter(src, index, num_nodes, aggr="mean")
-
-    assert mx.array_equal(
-        y_hat1, mx.array([0.5, 0.5, 1, 1])
-    ), "Simple scatter softmax failed"
-    assert mx.array_equal(y_hat2, mx.array([2, 1, 1])), "Simple scatter add failed"
-    assert mx.array_equal(y_hat3, mx.array([1, 1, 1])), "Simple scatter max failed"
-    assert mx.array_equal(y_hat4, mx.array([1, 1, 1])), "Simple scatter mean failed"
-
-
-def test_scatter_2D():
-    src = mx.array([[1.0, 2.0], [1.0, 3.0], [1.0, 4.0], [1.0, 5.0]])
-    index = mx.array([0, 0, 1, 2])
-    num_nodes = src.shape[0]
-
-    y_hat1 = scatter(src, index, aggr="softmax")
-
-    num_nodes = index.max().item() + 1
-    y_hat2 = scatter(src, index, num_nodes, aggr="add")
-    y_hat3 = scatter(src, index, num_nodes, aggr="max")
-    y_hat4 = scatter(src, index, num_nodes, aggr="mean")
-
-    y1 = mx.array([[0.5, 0.269], [0.5, 0.731], [1, 1], [1, 1]])
-    y2 = mx.array([[2, 5], [1, 4], [1, 5]])
-    y3 = mx.array([[1, 3], [1, 4], [1, 5]])
-    y4 = mx.array([[1, 2.5], [1, 4], [1, 5]])
-
-    assert mx.array_equal(
-        mx.round(y_hat1, 3), mx.round(y1, 3)
-    ), "scatter 2D softmax failed"
-    assert mx.array_equal(y_hat2, y2), "scatter 2D add failed"
-    assert mx.array_equal(y_hat3, y3), "scatter 2D max failed"
-    assert mx.array_equal(y_hat4, y4), "scatter 2D mean failed"
+    if aggr == "softmax":
+        assert mx.array_equal(
+            mx.round(y_hat, 3), mx.round(expected, 3)
+        ), f"Scatter {aggr} failed"
+    else:
+        assert mx.array_equal(y_hat, expected), f"Scatter {aggr} failed"
 
 
 def test_degree():
