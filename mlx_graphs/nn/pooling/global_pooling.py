4eb0932241685ab75066abb57b69ecbbc5217016
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-25 11:37:05 +0100

    add global_mean_pooling

--- mlx_graphs/nn/pooling/global_pooling.py
+++ mlx_graphs/nn/pooling/global_pooling.py
@@ -18,6 +18,9 @@ def global_add_pool(
         node_features (mx.array): Node features array.
         batch (mx.array, optional): Batch array of shape (node_features.shape[0]),
             indicating for each node its batch index.
+
+    Returns:
+        mx.array: An array with summed node features for all provided graphs.
     """
     if batch is None:
         return node_features.sum(axis=0, keepdims=True)
@@ -40,9 +43,37 @@ def global_max_pool(
         node_features (mx.array): Node features array.
         batch (mx.array, optional): Batch array of shape (node_features.shape[0]),
             indicating for each node its batch index.
+
+    Returns:
+        mx.array: An array with maximum node features for all provided graphs.
     """
     if batch is None:
         return node_features.max(axis=0, keepdims=True)
 
     out_size = batch.max().item() + 1
     return scatter(node_features, batch, out_size=out_size, axis=0, aggr="max")
+
+
+def global_mean_pool(
+    node_features: mx.array, batch: Optional[mx.array] = None
+) -> mx.array:
+    r"""Takes the feature-wise mean value along all node features to obtain
+    a global graph-level representation.
+
+    If `batch` is provided, applies pooling for each graph in the batch.
+    The returned shape is (1, node_features_dim) if `batch` is None, otherise
+    the shape is (num_batches, node_features_dim).
+
+    Args:
+        node_features (mx.array): Node features array.
+        batch (mx.array, optional): Batch array of shape (node_features.shape[0]),
+            indicating for each node its batch index.
+
+    Returns:
+        mx.array: An array with averaged node features for all provided graphs.
+    """
+    if batch is None:
+        return node_features.mean(axis=0, keepdims=True)
+
+    out_size = batch.max().item() + 1
+    return scatter(node_features, batch, out_size=out_size, axis=0, aggr="mean")
--- tests/nn/pooling/test_pooling.py
+++ tests/nn/pooling/test_pooling.py
@@ -1,9 +1,9 @@
 import mlx.core as mx
 
-from mlx_graphs.nn import global_add_pool, global_max_pool
+from mlx_graphs.nn import global_add_pool, global_max_pool, global_mean_pool
 
 
-def test_global_pooling():
+def test_global_add_pooling():
     node_features = mx.array([[1, 2], [3, 4], [5, 6]])
     batch = mx.array([0, 0, 1])
 
@@ -19,7 +19,11 @@ def test_global_pooling():
         y_hat2, mx.array([[4, 6], [5, 6]])
     ), "global_add_pool with batch failed"
 
-    # Max pooling
+
+def test_global_max_pooling():
+    node_features = mx.array([[1, 2], [3, 4], [5, 6]])
+    batch = mx.array([0, 0, 1])
+
     y_hat1 = global_max_pool(node_features)
     y_hat2 = global_max_pool(node_features, batch)
 
@@ -32,4 +36,22 @@ def test_global_pooling():
     ), "global_max_pool with batch failed"
 
 
-test_global_pooling()
+def test_global_mean_pooling():
+    node_features = mx.array([[1, 2], [3, 4], [5, 6]])
+    batch = mx.array([0, 0, 1])
+
+    y_hat1 = global_mean_pool(node_features)
+    y_hat2 = global_mean_pool(node_features, batch)
+
+    assert y_hat1.shape == [1, 2], "Simple global_mean_pool failed"
+    assert mx.array_equal(
+        y_hat1, mx.array([[(1 + 3 + 5) / 3, (2 + 4 + 6) / 3]])
+    ), "Simple global_mean_pool failed"
+
+    assert y_hat2.shape == [2, 2], "global_mean_pool with batch failed"
+    assert mx.array_equal(
+        y_hat2, mx.array([[(1 + 3) / 2, (2 + 4) / 2], [5, 6]])
+    ), "global_mean_pool with batch failed"
+
+
+test_global_mean_pooling()
