a035239c68c43aa597e5b5094f961d28e1fd82d4
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-01-28 20:08:47 +0100

    PR fix

--- mlx_graphs/data/data.py
+++ mlx_graphs/data/data.py
@@ -107,5 +107,5 @@ class GraphData:
             Incrementing value for the given attribute or None.
         """
         if "index" in key:
-            return len(self.node_features)
+            return len(self.node_features) if self.node_features else None
         return None
