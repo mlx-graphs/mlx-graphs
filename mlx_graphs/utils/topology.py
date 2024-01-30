fc92efd0a372c48fcfafe30bd2f399a790245c1a
Kheims <djamelmesbah141@gmail.com>
2024-01-30 17:58:35 +0100

    make eg code block

--- mlx_graphs/utils/topology.py
+++ mlx_graphs/utils/topology.py
@@ -77,19 +77,22 @@ def get_num_hops(model: nn.Module) -> int:
         number of hops the model is aggregating information
 
     Example:
-        >>> class GNN(nn.Module):
-        ...     def __init__(self):
-        ...         super().__init__()
-        ...         self.conv1 = GCNConv(4, 16)
-        ...         self.conv2 = GCNConv(16, 16)
-        ...         self.lin = nn.linear(16, 2)
-        ...
-        ...     def __call__(self, edge_index: mx.array, node_features: mx.array) -> mx.array:
-        ...         x = nn.relu(self.conv1(node_features, edge_index))
-        ...         x = self.conv2(node_features, edge_index)
-        ...         return self.lin(x)
-        >>> get_num_hops(GNN())
-        2
+
+    .. code-block:: python
+
+        class GNN(nn.Module):
+             def __init__(self):
+                 super().__init__()
+                 self.conv1 = GCNConv(4, 16)
+                 self.conv2 = GCNConv(16, 16)
+                 self.lin = nn.linear(16, 2)
+
+             def __call__(self, edge_index: mx.array, node_features: mx.array) -> mx.array:
+                 x = nn.relu(self.conv1(node_features, edge_index))
+                 x = self.conv2(node_features, edge_index)
+                 return self.lin(x)
+        get_num_hops(GNN())
+        # 2
     """
     from mlx_graphs.nn import MessagePassing
 
