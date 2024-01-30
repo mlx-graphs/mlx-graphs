c4670a07188e7ed92d06dcf1e6c83520e0586967
Kheims <djamelmesbah141@gmail.com>
2024-01-30 13:19:03 +0100

    additional test

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -6,9 +6,9 @@ from mlx_graphs.nn import GCNConv
 
 
 def test_get_num_hops():
-    class GCN(nn.Module):
+    class GNN_two_hops(nn.Module):
         def __init__(self):
-            super(GCN, self).__init__()
+            super(GNN_two_hops, self).__init__()
 
             self.conv1 = GCNConv(4, 16)
             self.conv2 = GCNConv(16, 16)
@@ -19,4 +19,13 @@ def test_get_num_hops():
             x = self.conv2(node_features, edge_index)
             return self.lin(x)
 
-    assert get_num_hops(GCN()) == 2, "get_num_hops failed"
+    class GNN_no_hops(nn.Module):
+        def __init__(self):
+            super(GNN_no_hops, self).__init__()
+            self.lin = nn.Linear(16, 2)
+
+        def __call__(self, edge_index: mx.array, node_features: mx.array) -> mx.array:
+            return self.lin(node_features)
+
+    assert get_num_hops(GNN_two_hops()) == 2, "get_num_hops failed"
+    assert get_num_hops(GNN_no_hops()) == 0, "get_num_hops failed"
