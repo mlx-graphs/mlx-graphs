1949ef4b529a656795c4abc55c728553e9b69ae3
Francesco Farina <farina.francesco1991@gmail.com>
2024-01-31 10:23:47 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-
+autodoc_mock_imports = ["mlx"]
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
