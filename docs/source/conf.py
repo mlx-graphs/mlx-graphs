5e11c94eae526745dd3bd5a8687495ae3b662297
Francesco Farina <farina.francesco1991@gmail.com>
2024-02-01 12:28:34 +0000

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ autodoc_mock_imports = ["mlx"]
 project = "mlx-graphs"
 copyright = "2024, mlx-graphs contributors"
 author = "mlx-graphs contributors"
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
