c6098a19688b5b4144b76f72e864ed3195bf66d9
Francesco Farina <farina.francesco1991@gmail.com>
2024-02-07 09:22:56 +0000

    migrate to mlx 0.1 (#70)

--- tests/nn/conv/test_gat_conv.py
+++ tests/nn/conv/test_gat_conv.py
@@ -8,41 +8,41 @@ mx.random.seed(42)
 def test_gat_conv():
     conv = GATConv(8, 20, heads=1)
 
-    node_features = mx.random.uniform(0, 1, (6, 8))
+    node_features = mx.random.uniform(0, 1, [6, 8])
     edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
     y_hat1 = conv(edge_index, node_features)
 
-    node_features = mx.random.uniform(-1, 1, (6, 8))
+    node_features = mx.random.uniform(-1, 1, [6, 8])
     edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
     y_hat2 = conv(edge_index, node_features)
 
     conv = GATConv(16, 32, heads=1)
-    node_features = mx.random.uniform(0, 1, (100, 16))
+    node_features = mx.random.uniform(0, 1, [100, 16])
     edge_index = mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]])
     y_hat3 = conv(edge_index, node_features)
 
     conv = GATConv(16, 32, heads=3, concat=True)
-    node_features = mx.random.uniform(0, 1, (100, 16))
+    node_features = mx.random.uniform(0, 1, [100, 16])
     edge_index = mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]])
     y_hat4 = conv(edge_index, node_features)
 
     conv = GATConv(16, 32, heads=3, concat=False)
-    node_features = mx.random.uniform(0, 1, (100, 16))
+    node_features = mx.random.uniform(0, 1, [100, 16])
     edge_index = mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]])
     y_hat5 = conv(edge_index, node_features)
 
     conv = GATConv(16, 32, heads=3, concat=False, edge_features_dim=10)
-    node_features = mx.random.uniform(0, 1, (100, 16))
-    edge_features = mx.random.uniform(0, 1, (5, 10))
+    node_features = mx.random.uniform(0, 1, [100, 16])
+    edge_features = mx.random.uniform(0, 1, [5, 10])
     edge_index = mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]])
     y_hat6 = conv(edge_index, node_features, edge_features=edge_features)
 
-    assert y_hat1.shape == [6, 20], "Simple GATConv failed"
-    assert y_hat2.shape == [6, 20], "GATConv with negative values failed"
-    assert y_hat3.shape == [100, 32], "GATConv with different shapes failed"
-    assert y_hat4.shape == [100, 32 * 3], "GATConv with multiple heads concat failed"
-    assert y_hat5.shape == [
+    assert y_hat1.shape == (6, 20), "Simple GATConv failed"
+    assert y_hat2.shape == (6, 20), "GATConv with negative values failed"
+    assert y_hat3.shape == (100, 32), "GATConv with different shapes failed"
+    assert y_hat4.shape == (100, 32 * 3), "GATConv with multiple heads concat failed"
+    assert y_hat5.shape == (
         100,
         32,
-    ], "GATConv with multiple heads without concat failed"
-    assert y_hat6.shape == [100, 32], "GATConv with edge features failed"
+    ), "GATConv with multiple heads without concat failed"
+    assert y_hat6.shape == (100, 32), "GATConv with edge features failed"
--- tests/nn/conv/test_sage_conv.py
+++ tests/nn/conv/test_sage_conv.py
@@ -10,30 +10,34 @@ from mlx_graphs.nn.conv.sage_conv import SAGEConv
         (
             SAGEConv(8, 20),
             mx.array([[0, 1, 2, 3], [0, 0, 1, 1]]),
-            mx.random.uniform(0, 1, (6, 8)),
+            mx.random.uniform(0, 1, [6, 8]),
             None,
-            [6, 20],
+            (6, 20),
         ),
         (
             SAGEConv(16, 32),
             mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]]),
-            mx.random.uniform(0, 1, (100, 16)),
+            mx.random.uniform(0, 1, [100, 16]),
             None,
-            [100, 32],
+            (100, 32),
         ),
         (
             SAGEConv(16, 32, bias=False),
             mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]]),
-            mx.random.uniform(0, 1, (100, 16)),
+            mx.random.uniform(0, 1, [100, 16]),
             None,
-            [100, 32],
+            (100, 32),
         ),
         (
             SAGEConv(16, 32, bias=False),
             mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]]),
-            mx.random.uniform(0, 1, (100, 16)),
-            mx.random.normal((5,)),
-            [100, 32],
+            mx.random.uniform(0, 1, [100, 16]),
+            mx.random.normal(
+                [
+                    5,
+                ]
+            ),
+            (100, 32),
         ),
     ],
 )
