108bbb58813eb680c9e75d4f7c350ed2f6aca5c0
Francesco Farina <farina.francesco1991@gmail.com>
2024-02-08 15:01:22 +0000

    add abstract methods and refactor

--- mlx_graphs/data/dataset.py
+++ mlx_graphs/data/dataset.py
@@ -1,50 +1,69 @@
 import os
+from abc import ABC, abstractmethod
 from typing import Optional
 
+DEFAULT_BASE_DIR = "~/.mlx_graphs_data/"
 
-class Dataset:
+
+class Dataset(ABC):
     """
     Base dataset class. ``download``, ``process``, and ``save`` methods must
     be implemented by children classes
 
     Args:
         name: name of the dataset
-        raw_dir: directory where to store raw data
-        save_dir: directory where to store processed data
+        base_dir: directory where to store raw and processed data. Default is
+            ``~/.mlx_graphs_data/``
     """
 
     def __init__(
         self,
         name: str,
-        download_url: Optional[str] = None,
-        raw_dir: str = ".local_data/raw",
-        save_dir: str = ".local_data/processed",
+        base_dir: Optional[str] = None,
     ):
-        self.name = name
-        self.dowload_url = download_url
-        self.raw_dir = raw_dir
-        self.save_dir = save_dir
-        self.raw_path = os.path.join(self.raw_dir, self.name)
-        self.save_path = os.path.join(self.save_dir, self.name)
+        self._name = name
+        self._base_dir = base_dir if base_dir is not None else DEFAULT_BASE_DIR
 
-        self.load()
+        self._load()
 
+    @property
+    def name(self):
+        """
+        Name of the dataset
+        """
+        return self._name
+
+    @property
+    def raw_path(self):
+        """
+        The path where raw files are stored. Defaults at `<base_dir>/<name>/raw`
+
+        """
+        return os.path.join(self._base_dir, self.name, "raw")
+
+    @property
+    def processed_path(self):
+        """
+        The path where raw files are stored. Defaults at `<base_dir>/<name>/processed`
+        """
+        return os.path.join(self._base_dir, self.name, "processed")
+
+    @abstractmethod
     def download(self):
-        raise NotImplementedError
+        """Download the dataset at `self.raw_path`."""
+        pass
+
+    @abstractmethod
+    def process(self):
+        """Process the dataset and possibly save it at `self.processed_path`"""
+        pass
 
     def _download(self):
         if os.path.exists(self.raw_path):
             return
-        os.makedirs(self.raw_dir)
+        os.makedirs(self._base_dir)
         self.download()
 
-    def process(self):
-        raise NotImplementedError
-
-    def save(self):
-        raise NotImplementedError
-
-    def load(self):
+    def _load(self):
         self._download()
         self.process()
-        self.save()
