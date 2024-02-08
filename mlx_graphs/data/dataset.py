5ae7380e40b8b58437cb4dd67a255e96f4d441f2
Francesco Farina <farina.francesco1991@gmail.com>
2024-02-08 10:58:07 +0000

    initial basic dataset class

---
+++ mlx_graphs/data/dataset.py
@@ -0,0 +1,45 @@
+import os
+
+
+class Dataset:
+    """
+    Base dataset class. ``download``, ``process``, and ``save`` methods must
+    be implemented by children classes
+
+    Args:
+        name: name of the dataset
+        raw_dir: directory where to store raw data
+        save_dir: directory where to store processed data
+    """
+
+    def __init__(
+        self,
+        name: str,
+        raw_dir: str = ".local_data/raw",
+        save_dir: str = ".local_data/processed",
+    ):
+        self.name = name
+        self.raw_dir = raw_dir
+        self.save_dir = save_dir
+        self.raw_path = os.path.join(self.raw_dir, self.name)
+        self.save_path = os.path.join(self.save_dir, self.name)
+
+    def download(self):
+        raise NotImplementedError
+
+    def _download(self):
+        if os.path.exists(self.raw_path):
+            return
+        os.makedirs(self.raw_dir)
+        self.download()
+
+    def process(self):
+        raise NotImplementedError
+
+    def save(self):
+        raise NotImplementedError
+
+    def load(self):
+        self._download()
+        self.process()
+        self.save()
