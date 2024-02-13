a7c3d70eb0bbae6b4da44111fb43b30445123e04
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-02-13 11:03:24 +0100

    add --datasets arg to pytest

--- tests/datasets/test_tu_dataset.py
+++ tests/datasets/test_tu_dataset.py
@@ -1,35 +1,44 @@
+import os
+import shutil
+
 import mlx.core as mx
+import pytest
 from torch_geometric.datasets import TUDataset as TUDataset_torch
 from torch_geometric.loader import DataLoader
 
 from mlx_graphs.data import Dataloader
 from mlx_graphs.datasets import TUDataset
-from mlx_graphs.datasets.dataset import DEFAULT_BASE_DIR
 
 
+@pytest.mark.slow
 def test_tu_dataset():
-    dataset_name = "REDDIT-BINARY"
-    dataset = TUDataset(dataset_name)
-    dataset_torch = TUDataset_torch(
-        DEFAULT_BASE_DIR + "tests/" + dataset_name, dataset_name
-    )
+    dataset_name = "ENZYMES"
+    path = os.path.join("/".join(__file__.split("/")[:-1]), ".tests/")
 
-    train_loader = Dataloader(dataset, 3, shuffle=False)
-    train_loader_torch = DataLoader(dataset_torch, 3, shuffle=False)
+    dataset = TUDataset(dataset_name, base_dir=path)
+    dataset_torch = TUDataset_torch(path, dataset_name)
+
+    train_loader = Dataloader(dataset, 10, shuffle=False)
+    train_loader_torch = DataLoader(dataset_torch, 10, shuffle=False)
 
     for batch_mxg, batch_pyg in zip(train_loader, train_loader_torch):
         assert mx.array_equal(
             mx.array(batch_pyg.edge_index.tolist()), batch_mxg.edge_index
         ), "Two arrays between PyG and mxg are different"
-        # assert mx.array_equal(
-        #     mx.array(batch_pyg.x.tolist()), batch_mxg.node_features
-        # ), "Two arrays between PyG and mxg are different"
-        assert mx.array_equal(
-            mx.array(batch_pyg.y.tolist()), batch_mxg.graph_labels
-        ), "Two arrays between PyG and mxg are different"
-        # assert mx.array_equal(
-        #     mx.array(batch_pyg.edge_attr.tolist()), batch_mxg.edge_features + 1
-        # ), "Two arrays between PyG and mxg are different"
 
+        if batch_mxg.node_features is not None:
+            assert mx.array_equal(
+                mx.array(batch_pyg.x.tolist()), batch_mxg.node_features
+            ), "Two arrays between PyG and mxg are different"
 
-test_tu_dataset()
+        if batch_mxg.graph_labels is not None:
+            assert mx.array_equal(
+                mx.array(batch_pyg.y.tolist()), batch_mxg.graph_labels
+            ), "Two arrays between PyG and mxg are different"
+
+        if batch_mxg.graph_labels is not None:
+            assert mx.array_equal(
+                mx.array(batch_pyg.y.tolist()), batch_mxg.graph_labels
+            ), "Two arrays between PyG and mxg are different"
+
+    shutil.rmtree(path)
