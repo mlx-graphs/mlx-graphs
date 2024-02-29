2ae3c21f09c110f2c5747ae650783abaecd7fe49
Michael Galkin <mikhgalkin@gmail.com>
2024-02-29 13:00:36 -0800

    Generalized Relational Conv layer and tests (#111)
    * Generalized Relational Conv layer and tests
    
    * fix GeneralizedRelationalConv import
    
    * scatten_min
    
    * test: no main
    
    * rel_conv fixes
    
    * fix typing
    
    * fix docstring and add to doc
    
    * refactoring
    
    * fixed rotate msg func, added a test
    
    * validate and renaming
    
    * docstring args fix and removed redundant kwargs set
    
    * made sure docstring is executable
    
    * fix comment:
    
    ---------
    
    Co-authored-by: Tristan Bilot <bilot.tristan@hotmail.fr>
    Co-authored-by: Francesco Farina <farina.francesco1991@gmail.com>

---
+++ tests/nn/conv/test_rel_conv.py
@@ -0,0 +1,74 @@
+import mlx.core as mx
+
+from mlx_graphs.nn.conv.rel_conv import GeneralizedRelationalConv
+
+mx.random.seed(42)
+
+
+def test_rel_conv():
+    conv = GeneralizedRelationalConv(8, 8, 4)
+
+    # 2D inputs (one graph)
+    node_features = mx.random.uniform(0, 1, [6, 8])
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    y_hat1 = conv(edge_index, node_features, edge_type, node_features)
+
+    # 3D inputs (bs separate graphs)
+    node_features = mx.random.uniform(0, 1, [3, 6, 8])
+    boundary = mx.zeros((3, 6, 8))
+    boundary[mx.array([0, 1, 2]), mx.array([0, 2, 3])] = 1.0
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    y_hat2 = conv(edge_index, node_features, edge_type, boundary)
+
+    # disconnected nodes
+    node_features = mx.random.uniform(0, 1, [100, 8])
+    edge_index = mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]])
+    edge_type = mx.array([0, 0, 1, 1, 2])
+    y_hat3 = conv(edge_index, node_features, edge_type, node_features)
+
+    # dependent = True
+    conv = GeneralizedRelationalConv(8, 8, 4, dependent=True)
+    node_features = mx.random.uniform(0, 1, [3, 6, 8])
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    query = mx.random.uniform(0, 1, [3, 8])
+    y_hat4 = conv(edge_index, node_features, edge_type, node_features, query)
+
+    # test 2D PNA aggregation
+    conv = GeneralizedRelationalConv(8, 8, 4, aggregate_func="pna")
+    node_features = mx.random.uniform(0, 1, [6, 8])
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    y_hat5 = conv(edge_index, node_features, edge_type, node_features)
+
+    # test 3D PNA aggregation
+    node_features = mx.random.uniform(0, 1, [3, 6, 8])
+    boundary = mx.zeros((3, 6, 8))
+    boundary[mx.array([0, 1, 2]), mx.array([0, 2, 3])] = 1.0
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    y_hat6 = conv(edge_index, node_features, edge_type, boundary)
+
+    # test rotate message function
+    conv = GeneralizedRelationalConv(8, 8, 4, message_func="rotate")
+    node_features = mx.random.uniform(0, 1, [3, 6, 8])
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+    edge_type = mx.array([0, 0, 1, 1])
+    y_hat7 = conv(edge_index, node_features, edge_type, node_features)
+
+    assert y_hat1.shape == (6, 8), "Simple GeneralizedRelationalConv failed"
+    assert y_hat2.shape == (3, 6, 8), "3D GeneralizedRelationalConv failed"
+    assert y_hat3.shape == (
+        100,
+        8,
+    ), "GeneralizedRelationalConv with different shapes failed"
+    assert y_hat4.shape == (
+        3,
+        6,
+        8,
+    ), "GeneralizedRelationalConv with dependent=True failed"
+    assert y_hat5.shape == (6, 8), "2D GeneralizedRelationalConv with PNA failed"
+    assert y_hat6.shape == (3, 6, 8), "3D GeneralizedRelationalConv with PNA failed"
+    assert y_hat7.shape == (3, 6, 8), "GeneralizedRelationalConv with rotate failed"
