06c8dda539e7f73115b665c583ea96c68a8b90c6
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2024-03-02 23:35:02 +0200

    Improve SimpleConv layer

--- mlx_graphs/nn/conv/simple_conv.py
+++ mlx_graphs/nn/conv/simple_conv.py
@@ -5,7 +5,7 @@ import mlx.core as mx
 from mlx_graphs.nn.message_passing import MessagePassing
 from mlx_graphs.utils import ScatterAggregations, add_self_loops
 
-CombineRootFunctions = Literal["add", "cat", "self_loop"]
+CombineRootFunctions = Literal["sum", "cat", "self_loop"]
 
 
 class SimpleConv(MessagePassing):
@@ -29,12 +29,13 @@ class SimpleConv(MessagePassing):
     ):
         super(SimpleConv, self).__init__(aggr, **kwargs)
 
-        if combine_root_func not in get_args(CombineRootFunctions):
+        if combine_root_func is not None and combine_root_func not in get_args(
+            CombineRootFunctions
+        ):
             raise ValueError(
                 "Invalid combine_root_func.",
                 f"Available values are {get_args(CombineRootFunctions)}",
             )
-
         self.combine_root_func = combine_root_func
 
     def __call__(
@@ -49,13 +50,20 @@ class SimpleConv(MessagePassing):
             edge_index: Input edge index of shape `[2, num_edges]`
             node_features: Input node features
             edge_weights: Edge weights leveraged in message passing. Default: ``None``
-
         Returns:
             mx.array: The computed node embeddings
         """
-
+        # Add self-loops, if needed.
         if self.combine_root_func == "self_loop":
-            edge_index = add_self_loops(edge_index)
+            # Edge weights exist.
+            if edge_weights is not None:
+                edge_index, edge_weights = add_self_loops(
+                    edge_index, edge_weights.reshape(-1, 1)
+                )
+                edge_weights = edge_weights.reshape(-1)
+            # Edge weights do not exist.
+            else:
+                edge_index = add_self_loops(edge_index)
 
         # Compute messages and aggregate them.
         output = self.propagate(
@@ -66,9 +74,9 @@ class SimpleConv(MessagePassing):
 
         # Combine the root node features.
         if self.combine_root_func is not None:
-            if self.combine_root_func == "add":
+            if self.combine_root_func == "sum":
                 output = output + node_features
             elif self.combine_root_func == "cat":
-                output = mx.cat([node_features, output], dim=-1)
+                output = mx.concatenate([node_features, output], axis=-1)
 
         return output
