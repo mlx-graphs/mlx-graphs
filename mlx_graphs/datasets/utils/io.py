2dd7b7d52e0fbd43999d47afbce422672f72ac06
Francesco Farina <farina.francesco1991@gmail.com>
2024-03-02 09:41:04 +0000

    TUDataset fix (#118)
    * process nodes and edge attributes
    
    * fix errors
    
    * fix parsing functions

--- mlx_graphs/datasets/utils/io.py
+++ mlx_graphs/datasets/utils/io.py
@@ -11,14 +11,12 @@ def is_floating_point(dtype: mx.Dtype) -> bool:
 
 def parse_txt_array(
     src: list[str],
-    sep: Optional[str] = None,
+    sep: str,
+    dtype: mx.Dtype,
     start: int = 0,
     end: Optional[int] = None,
-    dtype: Optional[mx.Dtype] = None,
-    device=None,
 ) -> mx.array:
     to_number = float if is_floating_point(dtype) else int
-
     parsed_src = [[to_number(x) for x in line.split(sep)[start:end]] for line in src]
     parsed_array = mx.array(parsed_src, dtype=dtype).squeeze()
     return parsed_array
@@ -26,12 +24,11 @@ def parse_txt_array(
 
 def read_txt_array(
     path: str,
-    sep: Optional[str] = None,
+    sep: str,
+    dtype: mx.Dtype,
     start: int = 0,
     end: Optional[int] = None,
-    dtype: Optional[mx.Dtype] = None,
-    device=None,
 ) -> mx.array:
     with open(path, "r") as f:
         src = f.read().split("\n")[:-1]
-    return parse_txt_array(src, sep, start, end, dtype, device)
+    return parse_txt_array(src, sep, dtype, start, end)
