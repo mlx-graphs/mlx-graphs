98c28966a97985a0f07ad3e4409d12c2e10d880e
Francesco Farina <farina.francesco1991@gmail.com>
2024-03-15 15:27:39 +0000

    slow test

--- tests/datasets/test_elliptic.py
+++ tests/datasets/test_elliptic.py
@@ -2,12 +2,13 @@ import os
 import shutil
 
 import mlx.core as mx
+import pytest
 
 from mlx_graphs.datasets import EllipticBitcoinDataset
 from mlx_graphs.loaders import Dataloader
 
 
-# @pytest.mark.slow
+@pytest.mark.slow
 def test_elliptic_bitcoin_dataset():
     from torch_geometric.datasets import (
         EllipticBitcoinDataset as EllipticBitcoinDataset_torch,
