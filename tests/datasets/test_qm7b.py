092bfe7cc41885b7ca38a3fa7a601b726b70c5fa
vinayhpandya <vinaydaredevil27@gmail.com>
2024-03-19 08:37:34 -0500

    Add temp_path fixture for deletion of files (#153)
    * Add temp_path fixture for deletion of files
    
    Added a temp_path fixture which will delete temporary files created
    while running tests.
    
    * refactor base dataset tests
    
    ---------
    
    Co-authored-by: vinayhpandya <vinayharshadpandya27@gmail.com>
    Co-authored-by: Francesco Farina <farina.francesco1991@gmail.com>

--- tests/datasets/test_qm7b.py
+++ tests/datasets/test_qm7b.py
@@ -1,6 +1,3 @@
-import os
-import shutil
-
 import mlx.core as mx
 import pytest
 
@@ -9,15 +6,12 @@ from mlx_graphs.loaders import Dataloader
 
 
 @pytest.mark.slow
-def test_qm7b_dataset():
+def test_qm7b_dataset(tmp_path):
     from torch_geometric.datasets import QM7b as QM7b_torch
     from torch_geometric.loader import DataLoader
 
-    path = os.path.join("/".join(__file__.split("/")[:-1]), ".tests/")
-    shutil.rmtree(path, ignore_errors=True)
-
-    dataset = QM7bDataset(base_dir=path)
-    dataset_torch = QM7b_torch(path)
+    dataset = QM7bDataset(base_dir=tmp_path)
+    dataset_torch = QM7b_torch(tmp_path)
 
     train_loader = Dataloader(dataset, 10, shuffle=False)
     train_loader_torch = DataLoader(dataset_torch, 10, shuffle=False)
@@ -36,5 +30,3 @@ def test_qm7b_dataset():
             assert mx.array_equal(
                 mx.array(batch_pyg.y.tolist()), batch_mxg.graph_labels
             ), "Two arrays between PyG and mxg are different"
-
-    shutil.rmtree(path)
