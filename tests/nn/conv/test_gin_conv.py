1563d2a562961c6a86f6f61cadf6d3f9ce660fcd
guillaume godin <guillaume.godin@gmail.com>
2024-03-25 13:26:58 +0100

    Add GINE capabilities to GIN (#145)
    * tentative GINE
    
    This is a tentative to implement GINE (adding edges)
    
    Current code needs to define the edges size maybe this can be avoid
    
    * implement GIN for edge features
    
    * fix GINE
    
    * update docs + remove bias arg
    
    * add gin example with or with edge features
    
    * Try Esol dataset
    
    * remove ESOL notebook
    
    * rm examples
    
    * typo
    
    * minor docs fix
    
    ---------
    
    Co-authored-by: TGG <guillaume.godin@firmenich.com>
    Co-authored-by: Tristan Bilot <bilot.tristan@hotmail.fr>
    Co-authored-by: Francesco Farina <farina.francesco1991@gmail.com>

--- tests/nn/conv/test_gin_conv.py
+++ tests/nn/conv/test_gin_conv.py
@@ -57,3 +57,58 @@ def test_gin_conv(layer, edge_index, node_features, edge_weights, expected):
     assert (
         expected == layer(edge_index, node_features, edge_weights=edge_weights).shape
     ), "GINConv failed"
+
+
+@pytest.mark.parametrize(
+    "layer, edge_index, node_features, edge_weights, edge_features, expected",
+    [
+        (
+            GINConv(
+                mlp=nn.Sequential(
+                    nn.Linear(16, 32),
+                    nn.ReLU(),
+                    nn.Linear(32, 32),
+                ),
+                node_features_dim=16,
+                edge_features_dim=10,
+            ),
+            mx.array([[0, 1, 2, 3], [0, 0, 1, 1]]),
+            mx.random.uniform(0, 1, [10, 16]),
+            None,
+            mx.random.normal((4, 10)),
+            (10, 32),
+        ),
+        (
+            GINConv(
+                mlp=nn.Sequential(
+                    nn.Linear(16, 32),
+                    nn.ReLU(),
+                    nn.Linear(32, 32),
+                ),
+                node_features_dim=16,
+                edge_features_dim=10,
+            ),
+            mx.array([[0, 1, 2, 3, 50], [0, 0, 1, 1, 99]]),
+            mx.random.uniform(0, 1, [100, 16]),
+            mx.random.normal(
+                [
+                    5,
+                ]
+            ),
+            mx.random.normal((5, 10)),
+            (100, 32),
+        ),
+    ],
+)
+def test_gine_conv(
+    layer, edge_index, node_features, edge_weights, edge_features, expected
+):
+    assert (
+        expected
+        == layer(
+            edge_index,
+            node_features,
+            edge_weights=edge_weights,
+            edge_features=edge_features,
+        ).shape
+    ), "GINEConv failed"
