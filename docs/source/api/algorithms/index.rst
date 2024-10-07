4619d97ade4a788d1fe20b4d08b994ad56ee5ca0
Vinay Pandya <vinayharshadpandya27@gmail.com>
2024-10-07 00:41:23 -0700

    feature : Implement node2vec algorithm (#170)
    * feature : Implement node2vec algorithm
    
    - Added mlx_cluster for fast random walk implementation
    - Implement node2vec algorithm
    
    * Change mlx_cluster dependency to 0.0.2
    
    - Chnaged dependency
    
    * FEATURE: Add node2vec tutorial notebook
    
    - update mlx-cluster and mlx
    - Add notebook on how to train a simple node2vec model
    
    * Updating `mlx-cluster` to 0.0.4 for node2vec
    
    * Remove types from function documentation
    
    * Remove node2vec from models to algorithms
    
    * mv node2vec to algorithms module and add docs
    
    ---------
    
    Co-authored-by: Francesco Farina <farina.francesco1991@gmail.com>

---
+++ docs/source/api/algorithms/index.rst
@@ -0,0 +1,13 @@
+.. _datasetslgorithms:
+
+Algorithms
+==========
+
+.. currentmodule:: mlx_graphs.algorithms
+
+
+.. autosummary::
+	:toctree: _autosummary
+	:template: custom-class-template.rst
+
+	Node2Vec
---
+++ tests/algorithms/test_node2vec.py
@@ -0,0 +1,20 @@
+import mlx.core as mx
+
+from mlx_graphs.algorithms import Node2Vec
+
+mx.random.seed(42)
+
+
+def test_node2vec():
+    edge_index = mx.array([[0, 1, 2, 3], [0, 0, 1, 1]])
+
+    embedding_size = 5
+    model = Node2Vec(
+        edge_index=edge_index,
+        embedding_dim=embedding_size,
+        walk_length=2,
+        context_size=1,
+        num_nodes=4,
+    )
+    embeddings = model(mx.arange(4).astype(mx.int64))
+    assert embeddings.shape == (4, 5), "Embedding dimensions are not equal"
